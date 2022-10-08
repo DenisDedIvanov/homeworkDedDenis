be85c375dfffc33f192f70c474220e13d5ea0f80
DenisDedIvanov <113638611+DenisDedIvanov@users.noreply.github.com>
2022-10-08 17:30:27 +0300

    1

---
+++ ДЗ3/complex.py
@@ -0,0 +1,17 @@
+import math
+class complex:
+
+    def __init__(self, re = 0, im = 0):
+        self.set(re , im)
+
+    def set(self, re, im):
+        self.a = re
+        self.b = im
+
+    def get(self):
+        return self.a , self.b
+
+
+
+
+
