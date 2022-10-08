9dec6bb8c7f7046263b9f66b4f0f6ba299b91ace
DenisDedIvanov <113638611+DenisDedIvanov@users.noreply.github.com>
2022-10-08 19:49:34 +0300

    1

--- ДЗ3/complex.py
+++ ДЗ3/complex.py
@@ -1,15 +1,27 @@
 import math
-class complex:
+class complexnum:
 
-    def __init__(self, re = 0, im = 0):
-        self.set(re , im)
+    def __init__(self, a = 0, b = 0):
+        self.set(a, b)
 
-    def set(self, re, im):
-        self.a = re
-        self.b = im
+    def set(self, a, b):
+        self.re = a
+        self.im = b
 
     def get(self):
-        return self.a , self.b
+        return self.re , self.im
+
+def sum(self, other):
+    return complexnum(self.re + other.re, self.im + other.im)
+z1 = complexnum(1, 2)
+z2 = complexnum(2, 3)
+z = sum(z1, z2)
+print(*z.get())
+
+
+
+
+
 
 
 
