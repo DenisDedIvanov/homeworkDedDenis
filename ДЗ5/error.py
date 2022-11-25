6a922f6baf6d90768d7d1b55917d3386db7e8831
DenisDedIvanov <113638611+DenisDedIvanov@users.noreply.github.com>
2022-11-25 21:18:42 +0300

    ирывмор

---
+++ ДЗ5/error.py
@@ -0,0 +1,107 @@
+import math
+import numbers
+class complexnum:
+
+    def __init__(self, a = 0, b = 0):
+        self.set(a, b)
+
+    def set(self, a, b):
+        if isinstance(a, numbers.Number) and isinstance(b, numbers.Number):
+            self.re = a
+            self.im = b
+        else:
+            raise ValueError
+
+    def get(self):
+        return self.re , self.im
+
+    def conv_alg_to_exp(self):
+        self.r = math.sqrt((self.re ** 2 + self.im ** 2))
+        self.phi = math.atan(self.re / self.im)
+        if self.re == 0:
+            raise Exception ('Данное число нельзя перевести в экспоненциальную форму. Введите другое число.')
+        elif self.re < 0 and self.im > 0:
+            self.phi = math.pi - self.phi
+        elif self.re < 0 and self.im < 0:
+            self.phi = math.pi + self.phi
+        elif self.re > 0 and self.im < 0:
+            self.phi = - self.phi
+        else:
+            self.phi = self.phi
+        self.re, self.im = self.r, self.phi
+        return
+
+    def conv_exp_to_alg(self):
+        self.re, self.im = self.re * math.cos(self.im), self.re * math.sin(self.im)
+        return
+
+    def __add__(self, other):
+        if isinstance(other, numbers.Number):
+            return self.re + other, self.im
+        else:
+            return self.re + other.re, self.im + other.im
+
+    def __radd__(self, other):
+        if isinstance(other, numbers.Number):
+            return self.re + other, self.im
+        return self.re + other.re, self.im + other.im
+
+    def __sub__(self, other):
+        if isinstance(other, numbers.Number):
+            return self.re - other, self.im
+        else:
+            return self.re - other.re, self.im - other.im
+
+    def __rsub__(self, other):
+        if isinstance(other, numbers.Number):
+            return other - self.re, self.im
+        else:
+            return other.re - self.re, other.im - self.im
+
+    def __mul__(self, other):
+        if isinstance(other, numbers.Number):
+            return self.re * other, self.im * other
+        else:
+            return self.re * other.re - self.im * other.im, self.re * other.im + self.im * other.re
+
+    def __rmul__(self, other):
+        if isinstance(other, numbers.Number):
+            return self.re * other, self.im * other
+        else:
+            return other.re * self.re - other.im * self.im, other.re * self.im + other.im * self.re
+
+    def __floordiv__(self, other):
+        if isinstance(other, numbers.Number):
+            return (self.re / other, self.im / other)
+        else:
+            return (self.re * other.re + self.im * other.im) / (other.re ** 2 + other.im ** 2), (self.im * other.re - self.re * other.im) / (other.re ** 2 + other.im ** 2)
+
+    def __rfloordiv__(self, other):
+        if isinstance(other, numbers.Number):
+            return (other / self.re, other / self.im)
+        return (other.re * self.re + other.im * self.im) / (self.re ** 2 + self.im ** 2), (other.im * self.re - other.re * self.im) / (self.re ** 2 + self.im ** 2)
+
+    def __getitem__(self, key):
+        if key == 0:
+            return self.re
+        if key == 1:
+            return self.im
+        else:
+            raise ValueError
+
+    def __setitem__(self, key, value):
+        if key == 0:
+            self.re = value
+        if key == 1:
+            self.im = value
+        else:
+            raise ValueError
+
+    def __eq__(self, other):
+        if isinstance(other, numbers.Number):
+            return (self.re == other and self.im == 0)
+        else:
+            return self.re == other.re and self.im == other.im
+
+    def __abs__(self):
+        return (self.re**2 + self.im**2)**0.5
