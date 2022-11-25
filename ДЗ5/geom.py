c56b832dc6c4709bd45202b6e70b7a08d10139de
DenisDedIvanov <113638611+DenisDedIvanov@users.noreply.github.com>
2022-11-25 22:29:54 +0300

    bjvds

--- ДЗ5/geom.py
+++ ДЗ5/geom.py
@@ -16,10 +16,10 @@ class Point:
         self._y = y
 
 def len(a,b):
-    return ((a.get_x() - b.get_x()) ** 2 + (a.get_y() - b.get_y()) ** 2) ** 0.5
+    return ((a.get_1() - b.get_1()) ** 2 + (a.get_2() - b.get_2()) ** 2) ** 0.5
 
 class Shape:
-    def __init__(self, type = '0'):
+    def __init__(self, type = 'Shape'):
         self._type = type
 
     def __str__(self):
@@ -36,11 +36,11 @@ class triangle(Shape):
         self._x3 = len(self._point1, self._point3)
 
     def perimeter(self):
-        return (self._x1 + self._x2 + self._x3)
+        return self._x1 + self._x2 + self._x3
 
     def area(self):
         pp = self.perimeter() / 2
-        return ((pp * (pp - self._x2) * (pp - self._x2) * (pp - self._x3)) ** 0.5)
+        return (pp * (pp - self._x2) * (pp - self._x2) * (pp - self._x3)) ** 0.5
 
 class circle(Shape):
     def __init__(self, o, r, type = "circle"):
@@ -49,7 +49,44 @@ class circle(Shape):
         self._o = o
 
     def area(self):
-        return round(3.14 * len(self._o, self._r) ** 2, 2)
+        return 3.14 * len(self._o, self._r) ** 2
 
     def perimeter(self):
-        return round( 2 * 3.14 * len(self._o, self._r), 2)
+        return 2 * 3.14 * len(self._o, self._r)
+class quadrangle(Shape):
+    def __init__(self, point1, point2, point3, point4, type = "quadrangle"):
+        super().__init__(type)
+        self._point1 = point1
+        self._point2 = point2
+        self._point3 = point3
+        self._point4 = point4
+        self._x1 = len(self._point1, self._point2)
+        self._x2 = len(self._point2, self._point3)
+        self._x3 = len(self._point3, self._point4)
+        self._x4 = len(self._point1, self._point4)
+
+    def perimeter(self):
+        return self._x1 + self._x2 + self._x3 + self._x4
+
+class rectangle(quadrangle):
+    def __init__(self, point1, point2, point3, point4, type = "rectangle"):
+        super().__init__(point1, point2, point3, point4, type)
+
+    def area(self):
+        return self._x1 * self._x2
+
+class square(rectangle):
+    def __init__(self, point1, point2, point3, point4, type = "square"):
+        super().__init__(point1, point2, point3, point4, type)
+
+class rhombus(quadrangle):
+    def __init__(self, point1, point2, point3, point4, type = "rhombus"):
+        super().__init__(point1, point2, point3, point4, type)
+
+    def area(self):
+        return 0.5 * len(self._point1, self._point3) * len(self._point2, self._point4)
+
+a = circle(Point(0,0), Point(0,1))
+b = square (Point(0,0), Point(0,5), Point(5,5), Point(5,0))
+print(a)
+print(b)
